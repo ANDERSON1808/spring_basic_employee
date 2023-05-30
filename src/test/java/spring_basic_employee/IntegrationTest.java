d2976881eed201cdba41b59c758dd9df7828dfa1
Anderson9520 <anderson.losada@experimentality.co>
2023-05-29 20:06:04 -0500

    Refactor code test

--- src/test/java/spring_basic_employee/IntegrationTest.java
+++ src/test/java/spring_basic_employee/IntegrationTest.java
@@ -1,11 +1,11 @@
 package spring_basic_employee;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Base composite annotation for integration tests.
@@ -13,6 +13,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(classes = SpringBasicEmployeeApplication.class)
-@ExtendWith(ReactiveSqlTestContainerExtension.class)
 public @interface IntegrationTest {
 }
